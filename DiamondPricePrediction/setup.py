0ec4e64780023c27a6f7ac97b6e28b4de0795c2c
p <p>
2026-02-13 16:28:05 +0530

    Final project structure with Flask app and templates

--- DiamondPricePrediction/setup.py
+++ DiamondPricePrediction/setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT='-e .'
+
+
 def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements :
+            requirements.remove(HYPHEN_E_DOT)
     return requirements
 
 setup(
@@ -13,6 +19,6 @@ setup(
     version='0.0.1',
     author='Prayag Raj',
     author_email='prayagbhatt49@gmail.com',
-    install_requires=get_requirements('requirements.txt'),
+    install_requires=get_requirements('Requirements.txt'),
     packages=find_packages()
 )
